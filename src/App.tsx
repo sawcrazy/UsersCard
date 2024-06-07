5b094bd6a5aab307d0e27586a4203b7356464cdd
Your Name <you@example.com>
2024-06-07 23:01:44 +0300

    addStyle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {Button} from "./components/Button/Button";
 import {Card} from "./components/Card/Card";
 import {IUser} from "./common/types/user";
+import './App.css'
 
 export const App = () => {
   const [users , setUsers] = useState<IUser[]>([]);
@@ -23,9 +24,13 @@ export const App = () => {
     return users.slice(page*5,(page+1)*5)
       .map((item) =>{
         return (
-          <Card key={item.id} user={item}/>
+          <Card key={item.id} user={item} delUser={delUser}/>
         )
       })
+  }
+  const delUser = (id) =>{
+    const deleteUser = users.filter((item) => item.id !== id);
+    setUsers(deleteUser);
   }
   const next = () =>{
     setPage(prev => prev + 1);
@@ -34,18 +39,22 @@ export const App = () => {
     setPage(prev => prev - 1);
   }
   return (
-    <div>
-      {renderUsers()}
-      {page !== 0 && (
-        <Button onClick={back}>
-          back
-        </Button>
-      )}
-      {users.length - ((page + 1)*5) > 0 && (
-        <Button onClick={next}>
-          next
-        </Button>
-      )}
+    <div className='main'>
+      <div className="main__userCard">
+        {renderUsers()}
+      </div>
+      <div className="footer__button">
+        {page !== 0 && (
+          <Button onClick={back}>
+            back
+          </Button>
+        )}
+        {users.length - ((page + 1)*5) > 0 && (
+          <Button onClick={next}>
+            next
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,48 @@
 import React, {FC} from "react";
 import s from './style.module.css';
 import {IUser} from "../../common/types/user";
+import {Button} from "../Button/Button";
 
 interface ICard {
   user: IUser;
 }
 
-export const Card: FC<ICard> = ({user}) => {
+export const Card: FC<ICard> = ({user,delUser}) => {
   return (
-    <div>
-      <ul>
-        <li>Имя : {user.name}</li>
-        <li>Никнейм : {user.username}</li>
-        <li>email : {user.email}</li>
-        <li>улица : {user.address.street}</li>
-        <li>suite : {user.address.suite}</li>
-        <li>город : {user.address.city}</li>
-        <li>zipcode : {user.address.zipcode}</li>
-        <li>lat : {user.address.geo.lat}</li>
-        <li>lng : {user.address.geo.lng}</li>
-        <li>телефон : {user.phone}</li>
-        <li>website : {user.website}</li>
-        <li>name : {user.company.name}</li>
-        <li>catchPhrase : {user.company.catchPhrase}</li>
-        <li>bs : {user.company.bs}</li>
-      </ul>
+    <div className={s.list}>
+      <dl className={s.holiday}>
+        <dt>Имя</dt>
+        <dd>{user.name}</dd>
+        <dt>Никнейм</dt>
+        <dd>{user.username}</dd>
+        <dt>Email</dt>
+        <dd>{user.email}</dd>
+        <dt>Улица</dt>
+        <dd>{user.address.street}</dd>
+        <dt>Suite</dt>
+        <dd>{user.address.suite}</dd>
+        <dt> Город</dt>
+        <dd>{user.address.city}</dd>
+        <dt>zipcode</dt>
+        <dd>{user.address.zipcode}</dd>
+        <dt>Долгота</dt>
+        <dd>{user.address.geo.lat}</dd>
+        <dt>Ширина</dt>
+        <dd>{user.address.geo.lng}</dd>
+        <dt>телефон</dt>
+        <dd>{user.phone}</dd>
+        <dt>website</dt>
+        <dd>{user.website}</dd>
+        <dt>Компания</dt>
+        <dd>{user.company.name}</dd>
+        <dt>catchPhrase</dt>
+        <dd>{user.company.catchPhrase}</dd>
+        <dt>BS</dt>
+        <dd>{user.company.bs}</dd>
+      </dl>
+        <Button onClick={()=>delUser(user.id)}>
+          удалить
+          </Button>
     </div>
   )
 }
